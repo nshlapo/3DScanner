e18080d6d1e3179780f62940310dfe9a0443b6ea
nshlapo <nur.shlapobersky@students.olin.edu>
2015-09-26 13:34:14 -0400

    Code

--- scanner.py
+++ scanner.py
@@ -4,40 +4,47 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
-ser= serial.Serial('/dev/ttyACM1')
+ser= serial.Serial('/dev/ttyACM0')
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# plt.ion()
-# plt.show()
 X = []
 Y = []
 Z = []
 now = time.time()
 future = now + 15
+plt.ion()
+plt.show()
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
 while True:
 	if time.time() > future:
 		break
 	try:
-		print 'Reading...'
 		line = ser.readline()
 		line = line[:-3]
+		print line
 		dis= line.split(',')
 		#print dis
-		theta = float(dis[0])
-		phi = float(dis[1])
+		theta = ((float(dis[0]))*math.pi)/180.0
+		phi = ((float(dis[1])+10)*math.pi)/180.0
+		# theta = ((float(dis[0])+60)*pi)/180.0
+		# phi = ((float(dis[1])-10)*pi)/180.0
 		r = float(dis[2])
-		x = r*cos(theta)
-		y = r*sin(theta)
+		x = r*cos(theta)*cos(phi)
+		y = r*sin(theta)*cos(phi)
 		z = r*sin(phi)
 		if z > 15:
 			X.append(x)
 			Y.append(y)
 			Z.append(z)
+			ax.scatter(x, y, z)
+			plt.draw()
 		else:
 			pass
 	except:
 		pass
-
+plt.ioff()
 ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
--- servomove/servomove.ino
+++ servomove/servomove.ino
@@ -19,39 +19,49 @@ void loop() {
   // topservo.write(180);
   // botservo.write(0);
 
-  for(i = 160; i >=130; i-=2) {
+  for(i = 160; i >=130; i-=1) {
     topservo.write(i);
-    delay(20);
-    if(i%4 == 0) {
-      for(j=12; j < 72; j+=4) {
-        int sensorValue = analogRead(A0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V) then to distance:
+    botservo.write(90);
+    // delay(20);
+    delay(300);
+    int sensorValue = analogRead(A0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V) then to distance:
         float voltage = sensorValue * (5.0 / 1023.0);
         float distance = 281.6588-438.174*voltage+306.2413*pow(voltage,2)-98.3268*pow(voltage,3)+11.6707*pow(voltage,4);
-        Serial.print(j);
+        Serial.print(90);
         Serial.print(",");
         Serial.print(i);
         Serial.print(",");
         Serial.println(distance);
+    // if(i%2 == 0) {
+    //   for(j=12; j < 72; j+=2) {
+    //     int sensorValue = analogRead(A0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V) then to distance:
+    //     float voltage = sensorValue * (5.0 / 1023.0);
+    //     float distance = 281.6588-438.174*voltage+306.2413*pow(voltage,2)-98.3268*pow(voltage,3)+11.6707*pow(voltage,4);
+    //     Serial.print(j);
+    //     Serial.print(",");
+    //     Serial.print(i);
+    //     Serial.print(",");
+    //     Serial.println(distance);
 
-        botservo.write(j);
-        delay(100);
-      }
-    }
-    else {
-      for(j=72; j > 12; j-=4) {
-        int sensorValue = analogRead(A0); // Read IR sensor (which goes from 0 - 1023) convert to a voltage (0 - 5V) then to distance:
-        float voltage = sensorValue * (5.0 / 1023.0);
-        float distance = 281.6588-438.174*voltage+306.2413*pow(voltage,2)-98.3268*pow(voltage,3)+11.6707*pow(voltage,4);
-        Serial.print(j);
-        Serial.print(",");
-        Serial.print(i);
-        Serial.print(",");
-        Serial.println(distance);
+    //     botservo.write(j);
+    //     delay(300);
+    //   }
+    // }
+    // else {
+    //   for(j=72; j > 12; j-=2) {
+    //     int sensorValue = analogRead(A0); // Read IR sensor (which goes from 0 - 1023) convert to a voltage (0 - 5V) then to distance:
+    //     float voltage = sensorValue * (5.0 / 1023.0);
+    //     float distance = 281.6588-438.174*voltage+306.2413*pow(voltage,2)-98.3268*pow(voltage,3)+11.6707*pow(voltage,4);
+    //     Serial.print(j);
+    //     Serial.print(",");
+    //     Serial.print(i);
+    //     Serial.print(",");
+    //     Serial.println(distance);
 
-        botservo.write(j);
-        delay(100);
-      }
-    }
+    //     botservo.write(j);
+    //     delay(300);
+    //   }
+    // }
   }
 }
 
